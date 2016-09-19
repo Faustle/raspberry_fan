62677652ad9d2ee0024e2f3ac73eb4b643420363
Faustle <azazello29@gmail.com>
2016-09-19 20:31:47 +0300

    Fixed some bugs

--- raspberry_fan.py
+++ raspberry_fan.py
@@ -6,15 +6,15 @@ import time
 import argparse
 
 ''' Parsing args '''
-parser = argparse.ArgumentParser()
-parser.add_argument('-H', '--hys', default=5, type=int,
-                    help='change hysteresis')
-parser.add_argument('-r', '--refresh', default=5, type=int,
-                    help='default refresh time')
-parser.add_argument('-m', '--max_temp', default=70, type=int,
-                    help='maximum temperature threshold')
-parser.add_argument('-d', '--disable_temp', default=47, type=int,
-                    help='fan disable temp')
+parser = argparse.ArgumentParser(description='PWM Auto adjust fan speed')
+parser.add_argument('-H', '--hys', default=5, type=int, metavar='N',
+                    help='hysteresis, default: 5\xb0C')
+parser.add_argument('-r', '--refresh', default=5, type=int, metavar='N',
+                    help='refresh rate, default: 5 sec')
+parser.add_argument('-m', '--max_temp', default=70, type=int, metavar='N',
+                    help='maximum temperature threshold, default: 70\xb0C')
+parser.add_argument('-d', '--turnoff', default=47, type=int, metavar='N',
+                    help='turn off fan at, default: 47\xb0C')
 parser.add_argument('-v', '--verbose', action='store_true',
                     help='print output')
 args = parser.parse_args()
@@ -25,22 +25,23 @@ args = vars(args)
 hys = args['hys']
 if hys < 1 or hys > 10:
     hys = 5
-    print("Hysteresis value not valid! Use default value (5)!")
+    print("Hysteresis value not valid! Default value (5) will be used!")
 
 ref = args['refresh']
 if ref < 1 or ref > 30:
     ref = 5
-    print("Refresh rate value not valid! Use default value (5)!")
+    print("Refresh rate value not valid! Default value (5) will be used!")
 
 m_temp = args['max_temp']
 if m_temp < 60 or m_temp > 80:
     m_temp = 70
-    print("Maximum temperature threshold value not valid! Use default value (70)!")
+    print("Maximum temperature threshold value not valid! Default value (70) \
+will be used!")
 
-d_temp = args['disable_temp']
+d_temp = args['turnoff']
 if d_temp < 40 or d_temp > 55:
     d_temp = 47
-    print("Disable fan value not valid! Use default value (47)!")
+    print("Turn off value not valid! Default value (47) will be used!")
 
 ''' Init temperature and fan vars '''
 fan_pin  = 1
@@ -76,9 +77,9 @@ def cputemp():
 temp = cputemp()
 fan(dict_table[temp])
 
-print('\nFAN CONTROL: Hysteresis: {0}\xb0C, Refresh rate: {1} sec, \
-Max temperature treshold: {2}\xb0C, \
-Disable fan at {3}\xb0C\n'.format(hys, ref, m_temp, d_temp))
+print('Current settings: Hysteresis: {0}\xb0C, Refresh rate: {1} sec, \
+Maximum temperature threshold: {2}\xb0C, \
+Turn off fan at {3}\xb0C\n'.format(hys, ref, m_temp, d_temp))
 
 while True:
     temp = cputemp()
@@ -90,5 +91,5 @@ while True:
         fan(0)
     if args['verbose']:
         print('Current = {0}\xb0C, Previus = {1}\xb0C, \
-Speed value = {2}, Hyteresys = {3}\xb0C'.format(temp, t, dict_table[temp], hys))
+Speed value = {2}, Hysteresis = {3}\xb0C'.format(temp, t, dict_table[temp], hys))
     time.sleep(ref)
